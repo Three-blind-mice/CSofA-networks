e2485e72adee8b6a92056be06fa87e0c0d6e0ed0
MarkHomeless <mr.rysistov@mail.ru>
2021-03-12 17:08:55 +0300

    initial commit

--- base/base_predictor.py
+++
@@ -1,7 +0,0 @@
-class BasePredictor(object):
-    def __init__(self, config):
-        self.config = config
-        self.model = None
-
-    def predict(self):
-        raise NotImplementedError
--- predictors/predictor.py
+++
@@ -1,7 +0,0 @@
-from base.base_predictor import BasePredictor
-
-
-class Predictor(BasePredictor):
-
-    def __init__(self, config, model):
-        super(Predictor, self).__init__(config, model)
