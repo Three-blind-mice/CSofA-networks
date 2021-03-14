4e5af3ab22ae1b459d5bf96eb24961a1b43fc57a
MarkHomeless <mr.rysistov@mail.ru>
2021-03-14 15:31:10 +0300

    initial commit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
     data_generator = DataGenerator(config)
     print('Created the data generator.')
     model = ConvModel(config)
-    if file in None:
+    if file is None:
         model.load(file)
     print('Created the model.')
     trainer = ConvModelTrainer(config, model.model)
