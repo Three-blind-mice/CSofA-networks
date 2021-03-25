c08a09c0fde369302b1f18c4821c1c4c3c9021c3
MarkHomeless <mr.rysistov@mail.ru>
2021-03-25 12:47:25 +0300

    initial commit

--- trainers/conv_model_trainer.py
+++ trainers/conv_model_trainer.py
@@ -31,7 +31,6 @@ class ConvModelTrainer(BaseTrain):
                 self.model.load_weights(os.path.join(self.config.callbacks.checkpoint.dir, 'best_model.hdf5'))
                 self.model.save(os.path.join(self.config.callbacks.checkpoint.dir, 'model_step-{}.hdf5'.format(step)))
                 self.config.model.optimizer.params.learning_rate /= self.config.trainer.learning_rate_factor
-                #backend.set_value(self.model.optimizer.learning_rate, self.config.trainer.learning_rate_factor * self.model.optimizer.learning_rate)
                 print(f'Learning rate: {self.model.optimizer.learning_rate.numpy()}')
                 print(f"Fine tuning step: {step} was completed in {((time.time()-start_time)/60):.2f} min\n")
         elif self.config.trainer.mode.lower() == 'without_fine_tuning':
